6b22892c4ad1b01f7db637433561448559654682
Antriksh Gupta <guptaantriksh12@gmail.com>
2023-06-20 15:38:08 +0530

    added authentication

--- app.py
+++ app.py
@@ -2,18 +2,41 @@ import sqlite3
 from hashids import Hashids
 from flask import Flask, render_template, request, flash, redirect, url_for
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, LoginManager, login_user, login_required, current_user, logout_user
 
 app = Flask(__name__)
-# antrikhs
+app.config['SECRET_KEY'] = 'this should be a secret random string'
+hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     return connection
 
-app.config['SECRET_KEY'] = 'this should be a secret random string'
-hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
+class User(UserMixin):
+    def __init__(self, user_id, username=None):
+        self.id = user_id
+        self.username = username
 
-@app.route('/', methods=('GET', 'POST'))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    connection = get_db_connection()
+    user_data = connection.execute('SELECT id, username FROM users WHERE id = ?', (user_id,)).fetchone()
+    connection.close()
+    if user_data:
+        return User(user_data['id'], user_data['username'])
+    return None
+
+
+@app.route('/', methods=['GET', 'POST'])
+@login_required
 def index():
     connection = get_db_connection()
 
@@ -26,7 +49,7 @@ def index():
         notes = request.form['notes']
 
         if not url:
-            flash('The URL is required!')
+            flash('The URL is required!', 'error')
             return redirect(url_for('index'))
 
         existing_url = connection.execute(
@@ -61,12 +84,14 @@ def index():
                     (alias,)
                 ).fetchone()
                 if existing_alias:
-                    flash('The alias is already used. Please choose a different one.')
+                    flash('The alias is already used. Please choose a different one.', 'error')
                     return redirect(url_for('index'))
 
+            user_id = current_user.id
+
             url_data = connection.execute(
-                'INSERT INTO urls (original_url, alias, notes) VALUES (?, ?, ?)',
-                (url, alias, notes)
+                'INSERT INTO urls (original_url, alias, notes, user_id) VALUES (?, ?, ?, ?)',
+                (url, alias, notes, user_id)
             )
             connection.commit()
             connection.close()
@@ -79,10 +104,77 @@ def index():
                 hashid = hashids.encode(url_id)
                 short_url = request.host_url + hashid
 
-        return render_template('index.html', short_url=short_url,  current_year=current_year, author_name=author_name)
+        flash('URL shortened successfully!', 'success')
+        return render_template('index.html', short_url=short_url, current_year=current_year, author_name=author_name)
 
     return render_template('index.html', current_year=current_year, author_name=author_name)
 
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    current_year = datetime.now().year
+    author_name = "Antriksh Gupta"
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('All fields are required!', 'error')
+            return redirect(url_for('register'))
+
+        connection = get_db_connection()
+        existing_user = connection.execute('SELECT id FROM users WHERE username = ?', (username,)).fetchone()
+        if existing_user:
+            flash('Username already exists. Please choose a different one.', 'error')
+            connection.close()
+            return redirect(url_for('register'))
+
+        # hash the password 
+        hashed_password = generate_password_hash(password)
+        connection.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, hashed_password))
+        connection.commit()
+        connection.close()
+
+        flash('Registration successful. You can now log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', current_year=current_year, author_name=author_name)
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+
+    current_year = datetime.now().year
+    author_name = "Antriksh Gupta"
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('All fields are required!', 'error')
+            return redirect(url_for('login'))
+
+        connection = get_db_connection()
+        user_data = connection.execute('SELECT id, password FROM users WHERE username = ?', (username,)).fetchone()
+        connection.close()
+
+        if user_data and check_password_hash(user_data['password'], password):
+            user = User(user_data['id'])
+            login_user(user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password.', 'error')
+            return redirect(url_for('login'))
+
+    return render_template('login.html', current_year=current_year, author_name=author_name)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('index'))
 
 @app.route('/<alias>')
 def url_redirect(alias):
@@ -97,35 +189,32 @@ def url_redirect(alias):
         original_url = url_data['original_url']
         clicks = url_data['clicks']
 
-        conn.execute('UPDATE urls SET clicks = ? WHERE alias = ?',
-                     (clicks + 1, alias))
-
+        conn.execute('UPDATE urls SET clicks = ? WHERE alias = ?', (clicks + 1, alias))
         conn.commit()
         conn.close()
 
         return redirect(original_url)
     else:
-        flash('Invalid URL')
+        flash('Invalid URL', 'error')
         return redirect(url_for('index'))
 
 
 @app.route('/stats')
+@login_required
 def stats():
     conn = get_db_connection()
-
-    search_query = request.args.get('q')  # Retrieve search query from the URL parameter 'q'
+    search_query = request.args.get('q')
 
     if search_query:
-        # Search URLs based on notes or original URL content
         db_urls = conn.execute(
             'SELECT id, created, original_url, alias, clicks, notes FROM urls'
-            ' WHERE notes LIKE ? OR original_url LIKE ?',
-            ('%' + search_query + '%', '%' + search_query + '%')
+            ' WHERE (notes LIKE ? OR original_url LIKE ?) AND user_id = ?',
+            ('%' + search_query + '%', '%' + search_query + '%', current_user.id)
         ).fetchall()
     else:
-        # Retrieve all URLs if no search query is provided
         db_urls = conn.execute(
-            'SELECT id, created, original_url, alias, clicks, notes FROM urls'
+            'SELECT id, created, original_url, alias, clicks, notes FROM urls WHERE user_id = ?',
+            (current_user.id,)
         ).fetchall()
 
     conn.close()
@@ -138,7 +227,7 @@ def stats():
 
     current_year = datetime.now().year
     author_name = "Antriksh Gupta"
-    return render_template('stats.html', urls=urls, search_query=search_query, current_year=current_year, author_name=author_name)
+    return render_template('stats.html', urls=urls, search_query=search_query, current_year=current_year, author_name=author_name, username=current_user.username)
 
 @app.route('/search')
 def search():
--- schema.sql
+++ schema.sql
@@ -1,10 +1,18 @@
 DROP TABLE IF EXISTS urls;
 
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
 CREATE TABLE urls (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     original_url TEXT NOT NULL,
     clicks INTEGER NOT NULL DEFAULT 0,
     notes TEXT,
-    alias TEXT UNIQUE
+    alias TEXT UNIQUE,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
